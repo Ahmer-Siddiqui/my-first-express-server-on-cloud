c18936bbc32deaf35e8e71dfe30b33963520eea4
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-04-01 17:32:58 +0500

    change code with class code

--- server.mjs
+++ server.mjs
@@ -1,16 +1,12 @@
-console.log("I am server file");
-
-import express from 'express'
-// const express = require('express') // old so reuired not allow
+import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    console.log("request ip:" , req.ip);
-  res.send('Hello World!' + new Date())
-  res.send('Hello World! ' + new Date().toString());
+    console.log("request ip: ", req.ip);
+    res.send('Hello World! ' + new Date().toString());
 })
-
+// http://172.16.23.138:3000
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-}  )
+    console.log(`Example app listening on port ${port}`)
+})
