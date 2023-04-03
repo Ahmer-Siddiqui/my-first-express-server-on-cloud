3db6a4adfb07519d985775b5c85c8776c36e4c57
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-04-03 21:37:02 +0500

    static page in server

--- server.mjs
+++ server.mjs
@@ -1,12 +1,19 @@
-import express from 'express';
+import path from 'path';
+import express from 'express'
+// const express = require('express') // old so reuired not allow
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    console.log("request ip: ", req.ip);
-    res.send('Hello World! ' + new Date().toString());
+app.get('/abc', (req, res) => {
+    console.log("request ip:" , req.ip);
+  res.send('Hello World!' + new Date())
+  res.send('Hello World! ' + new Date().toString());
 })
-// http://172.16.23.138:3000
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname ,'./web/build')))
+
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`)
+}  )
