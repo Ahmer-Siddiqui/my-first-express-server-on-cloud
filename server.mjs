7d44da239947958d82967f1a9c96dd6ae474f9ed
Ahmer Siddiqui <siddiquiahmer970@gmail.com>
2023-04-03 21:53:35 +0500

    single page hosting technic

--- server.mjs
+++ server.mjs
@@ -5,13 +5,21 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/abc', (req, res) => {
-    console.log("request ip:" , req.ip);
-  res.send('Hello World!' + new Date())
+  console.log("request ip:" , req.ip);
+  res.send('Hello World! ' + new Date().toString());
+})
+app.get('/getweather', (req, res) => {
+  console.log("request ip:" , req.ip);
+  res.send('Hello World! ' + new Date().toString());
+})
+app.get('/gettime', (req, res) => {
+  console.log("request ip:" , req.ip);
   res.send('Hello World! ' + new Date().toString());
 })
 
 const __dirname = path.resolve();
 app.use('/', express.static(path.join(__dirname ,'./web/build')))
+app.use('*', express.static(path.join(__dirname ,'./web/build')))
 
 
 app.listen(port, () => {
